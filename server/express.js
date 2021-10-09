c5afe09828830cfa877fb1992246f3f139d0eaa3
DoanManhHieu <hieudoanmanh.it@gmail.com>
2021-10-09 23:14:38 +0700

    cau hinh lai

---
+++ server/express.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import template from './../template'
+
+const app=express()
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure apps by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send(template())
+})
+
+export default app
